c496877eec2239509e0158aefbc0fb70eca8e23a
Jake Suddreth <58892456+jsuddret@users.noreply.github.com>
2020-05-17 12:22:44 -0700

    Add files via upload

--- Beam.py
+++ Beam.py
@@ -6,24 +6,18 @@ class PositiveVerticalPointLoad:
     force = 0
     distance = 0
 
-    def __init__(self, stage):
-        c = Canvas(stage, height=100, width=51)
-        force_input = Entry(c, width=5, bg='light gray')
-        force_input.place(x=2, y=65)
-        distance_input = Entry(c, width=3, bg='light gray')
-        distance_input.place(x=16, y=85)
+    def __init__(self, count):
+        c = Canvas(window, height=100, width=51)
         c.pack(expand=1, fill=BOTH)
-        c.place(x=950, y=50)
-        c.create_line(27, 10, 27, 60, width=4)
+        c.place(x=925, y=50)
+        c.create_line(27, 10, 27, 90, width=4)
         c.create_line(17, 30, 27, 11, width=4)
         c.create_line(37, 30, 27, 11, width=4)
-        c.create_text(45, 76, text='N')
-        c.create_text(8, 95, text='@')
-        c.create_text(45, 95, text='m')
+        id_num = count
+        c.bind('<Double-Button-1>', on_double_click_positive_point_load(id_num))
         c.bind(w, drag)
 
     def getForce(self):
-        self.force = force_input.get()
         return self.force
 
     def setForce(self, F):
@@ -42,18 +36,11 @@ class NegativeVerticalPointLoad:
 
     def __init__(self):
         c = Canvas(window, height=100, width=51)
-        force_input = Entry(c, width=5, bg='light gray')
-        force_input.place(x=2, y=3)
-        distance_input = Entry(c, width=3, bg='light gray')
-        distance_input.place(x=16, y=22)
         c.pack(expand=1, fill=BOTH)
-        c.place(x=950, y=50)
-        c.create_line(27, 45, 27, 95, width=4)
+        c.place(x=925, y=50)
+        c.create_line(27, 10, 27, 95, width=4)
         c.create_line(17, 75, 27, 96, width=4)
         c.create_line(37, 75, 27, 96, width=4)
-        c.create_text(45, 10, text='N')
-        c.create_text(8, 30, text='@')
-        c.create_text(45, 30, text='m')
         c.bind(w, drag)
 
     def getForce(self):
@@ -75,27 +62,20 @@ class PositivePointMomentLoad:
 
     def __init__(self):
         c = Canvas(window, height=100, width=51)
-        magnitude_input = Entry(c, width=3, bg='light gray')
-        magnitude_input.place(x=2, y=65)
-        distance_input = Entry(c, width=3, bg='light gray')
-        distance_input.place(x=16, y=85)
         c.pack(expand=1, fill=BOTH)
-        c.place(x=950, y=50)
-        c.create_arc(35, 60, 10, 15, start=-65, extent=180, width=4)
-        c.create_line(28, 59, 17, 17, fill='SystemButtonFace', width=4)
-        c.create_line(13, 16, 28, 28, width=4)
-        c.create_line(13, 17, 28, 6, width=4)
-        c.create_oval(20, 35, 25, 40, fill='black')
-        c.create_text(40, 76, text='N*m')
-        c.create_text(8, 95, text='@')
-        c.create_text(45, 95, text='m')
+        c.place(x=925, y=50)
+        c.create_arc(35, 90, 10, 20, start=-95, extent=180, width=4)
+        c.create_line(21, 90, 24, 20, fill='SystemButtonFace', width=4)
+        c.create_line(26, 20, 44, 34, width=4)
+        c.create_line(26, 20, 22, 44, width=4)
+        c.create_oval(20, 52, 25, 57, fill='black')
         c.bind(w, drag)
 
-    def getMagnitude(self):
+    def getForce(self):
         return self.magnitude
 
-    def setMagnitude(self, m):
-        self.magnitude = m
+    def setForce(self, F):
+        self.magnitude = F
 
     def getDistance(self):
         return self.distance
@@ -110,27 +90,20 @@ class NegativePointMomentLoad:
 
     def __init__(self):
         c = Canvas(window, height=100, width=51)
-        magnitude_input = Entry(c, width=3, bg='light gray')
-        magnitude_input.place(x=2, y=65)
-        distance_input = Entry(c, width=3, bg='light gray')
-        distance_input.place(x=16, y=85)
         c.pack(expand=1, fill=BOTH)
-        c.place(x=950, y=50)
-        c.create_arc(10, 15, 35, 60, start=65, extent=180, width=4)
-        c.create_line(28, 17, 17, 59, fill='SystemButtonFace', width=4)
-        c.create_line(28, 16, 17, 28, width=4)
-        c.create_line(28, 17, 17, 6, width=4)
-        c.create_oval(20, 35, 25, 40, fill='black')
-        c.create_text(40, 76, text='N*m')
-        c.create_text(8, 95, text='@')
-        c.create_text(45, 95, text='m')
+        c.place(x=925, y=50)
+        c.create_arc(40, 90, 15, 20, start=95, extent=180, width=4)
+        c.create_line(26, 21, 29, 95, fill='SystemButtonFace', width=4)
+        c.create_line(25, 19, 28, 42, width=4)
+        c.create_line(25, 19, 8, 30, width=4)
+        c.create_oval(25, 52, 30, 57, fill='black')
         c.bind(w, drag)
 
-    def getMagnitude(self):
+    def getForce(self):
         return self.magnitude
 
-    def setMagnitude(self, m):
-        self.magnitude = m
+    def setForce(self, F):
+        self.magnitude = F
 
     def getDistance(self):
         return self.distance
@@ -139,12 +112,43 @@ class NegativePointMomentLoad:
         self.distance = d
 
 
+def on_double_click_positive_point_load(identification_number):
+    data_window = Tk()
+    data_window.geometry('360x240+560+240')
+    data_window.configure(bg='light grey')
+    c = Canvas(data_window, height=240, width=360)
+    c.pack()
+    c.create_text(120, 60, text='Force: ')
+    force_input = Entry(c, width=12)
+    force_input.place(x=150, y=50)
+    c.create_text(245, 60, text='N')
+    c.create_text(127, 90, text='Distance: ')
+    distance_input = Entry(c, width=12)
+    distance_input.place(x=160, y=80)
+    c.create_text(254, 90, text='m')
+    enter_button = Button(c, text='Enter', command=lambda: enter_pressed(identification_number, force_input,
+                                                                         distance_input))
+    enter_button.place(x=180, y=125, anchor=CENTER)
+    data_window.mainloop()
+
+
 def drag(event):
     event.widget.place(x=event.x_root - 360, y=event.y_root - 110, anchor=CENTER)
 
 
+def enter_pressed(identification_number, f_in, d_in):
+    try:
+        print(loads_list)
+        loads_list[0][identification_number-1].setForce(f_in.get())
+        loads_list[0][identification_number-1].setDistance(d_in.get())
+    except ValueError:
+        pass
+
+
 def positive_vertical_point_load_pushed():
-    pvpl = PositiveVerticalPointLoad(window)
+    global positive_vertical_point_count
+    positive_vertical_point_count += 1
+    pvpl = PositiveVerticalPointLoad(positive_vertical_point_count)
     positive_vertical_point_load_list.append(pvpl)
 
 
@@ -185,19 +189,20 @@ length_input.place(x=425, y=120)
 length = 0
 w = "<B1-Motion>"
 positive_vertical_point_load_list = []
+positive_vertical_point_count = 0
 negative_vertical_point_load_list = []
 positive_point_moment_list = []
 negative_point_moment_list = []
 loads_list = [positive_vertical_point_load_list, negative_vertical_point_load_list,
               positive_point_moment_list, negative_point_moment_list]
-positive_point_load_button = Button(window, bg='light gray', text='Point Load (+)', fg='black', width=15,
+positive_point_load_button = Button(window, bg='light grey', text='Point Load (+)', fg='black', width=15,
                                     command=positive_vertical_point_load_pushed).place(x=1070, y=50)
-negative_point_load_button = Button(window, bg='light gray', text='Point Load (-)', fg='black', width=15,
+negative_point_load_button = Button(window, bg='light grey', text='Point Load (-)', fg='black', width=15,
                                     command=negative_vertical_point_load_pushed).place(x=1070, y=85)
-positive_point_moment_button = Button(window, bg='light gray', text='Point Moment (+)', fg='black', width=15,
+positive_point_moment_button = Button(window, bg='light grey', text='Point Moment (+)', fg='black', width=15,
                                       command=positive_point_moment_pushed).place(x=1070, y=120)
-negative_point_moment_button = Button(window, bg='light gray', text='Point Moment (-)', fg='black', width=15,
+negative_point_moment_button = Button(window, bg='light grey', text='Point Moment (-)', fg='black', width=15,
                                       command=negative_point_moment_pushed).place(x=1070, y=155)
-calculate_button = Button(window, bg='light gray', text='Calculate', fg='black', width=15,
+calculate_button = Button(window, bg='light grey', text='Calculate', fg='black', width=15,
                           command=calculate_pressed).place(x=1070, y=190)
 window.mainloop()
